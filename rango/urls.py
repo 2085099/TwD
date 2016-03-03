26d88bafc5e15e68e03551d2e621acd42973646d
2085099 <2085099M@student.gla.ac.uk>
2016-03-03 14:48:44 +0000

    03/03

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^category/(?P<category_name_slug>\w+)$', views.category, name='category'),
         url(r'^add_category/$', views.add_category, name='add_category'),
-
         url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!
         url(r'^login/$', views.user_login, name='login'),
         url(r'^restricted/', views.restricted, name='restricted'),
